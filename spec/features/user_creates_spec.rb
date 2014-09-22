1b5693b8b1e551a1ecf0286aae87469fa412b611
Joe Grady <jogrady1@gmail.com>
2014-09-22 17:21:19 -0400

    Almost working second test

--- spec/features/user_creates_spec.rb
+++ spec/features/user_creates_spec.rb
@@ -9,20 +9,30 @@ Create matchup
 }  do
 
 scenario "User provides required information" do
+
  visit root_path
- click_on "Sign up"
+
+ click_on "sign up"
  fill_in "Email", with: "abc@example.com"
- fill_in "Password", with: "123456"
- fill_in "Password confirmation", with: "123456"
+ fill_in "Password", with: "12345678"
+ fill_in "Password confirmation", with: "12345678"
+q1
  within '.new_user' do
-  click_on "sign up"
+  click_on "Sign up"
 end
-expect(page).to have_content "Welcome!You have signed up successfully"
+expect(page).to have_content "Welcome! You have signed up successfully."
 end
 
-scenario
-
-
+scenario "User doesn't provide required information" do
+visit root_path
+click_on "Sign up"
+within '.new_user' do
+  click_on "Sign up"
+end
+expect(page).to have_content "Email can't be blank"
+expect(page).to have_content "Password can't be blank (8 characters minimum)"
+expect(page).to have_content "2 errors  prohibited this user form being saved: Email can't be blank"
+end
 
 end
 
