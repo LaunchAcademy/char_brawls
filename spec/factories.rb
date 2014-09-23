86f7a72d1db2b36270a4450cece7c85c8c39b991
rachelbookhout <rbookhou@gmail.com>
2014-09-23 13:51:24 -0400

    added login tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl. define do
+  factory :user do
+    first_name "Humphrey"
+    last_name "Bogart"
+    email "crazyemail@something.gov"
+    password "woooooooo2"
+  end
+end
--- spec/features/user_creates_spec.rb
+++ spec/features/user_creates_spec.rb
@@ -1,38 +1,116 @@
 require 'rails_helper'
 feature "User creates a new account", %Q{
-As an authenticated user I want to:
-Create matchup
- Write an opinion
- Vote on the outcome of a matchup
- Edit their own review
- See sign-out button
+As an user,
+I want to create an account
+and be asked to sign in again once logged in
 }  do
 
 scenario "User provides required information" do
+  user = FactoryGirl.build(:user)
 
- visit root_path
-
- click_on "sign up"
- fill_in "Email", with: "abc@example.com"
- fill_in "Password", with: "12345678"
- fill_in "Password confirmation", with: "12345678"
-q1
- within '.new_user' do
+  visit root_path
   click_on "Sign up"
-end
-expect(page).to have_content "Welcome! You have signed up successfully."
+
+  fill_in "First name", with: user.first_name
+
+  fill_in "Last name", with: user.last_name
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  fill_in "Password confirmation", with: user.password
+  within '.new_user' do
+    click_on "Sign up"
+  end
+
+  expect(page).to have_content "Welcome! You have signed up successfully."
+
 end
 
 scenario "User doesn't provide required information" do
-visit root_path
-click_on "Sign up"
-within '.new_user' do
+  visit root_path
   click_on "Sign up"
+  within '.new_user' do
+    click_on "Sign up"
+  end
+  expect(page).to have_content "Email can't be blank"
+  expect(page).to have_content "Password can't be blank"
+  expect(page).to have_content "2 errors prohibited this user from being saved: Email can't be blank"
 end
-expect(page).to have_content "Email can't be blank"
-expect(page).to have_content "Password can't be blank (8 characters minimum)"
-expect(page).to have_content "2 errors  prohibited this user form being saved: Email can't be blank"
+
+scenario "Email is already in use" do
+  user = FactoryGirl.create(:user)
+
+  visit root_path
+  click_on "Sign up"
+
+
+  fill_in "First name", with: user.first_name
+  fill_in "Last name", with: user.last_name
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  fill_in "Password confirmation", with: user.password
+
+  within '.new_user' do
+    click_on "Sign up"
+  end
+
+  expect(page).to have_content "Email has already been taken"
+
 end
+
+
+
+scenario "Passwords don't match" do
+
+  user = FactoryGirl.build(:user)
+
+  visit root_path
+  click_on "Sign up"
+
+
+  fill_in "First name", with: user.first_name
+  fill_in "Last name", with: user.last_name
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  fill_in "Password confirmation", with: "fpfejfkef"
+
+  within '.new_user' do
+    click_on "Sign up"
+  end
+
+
+  expect(page).to have_content "Password confirmation doesn't match"
+
+end
+
+
+scenario "User is signed in" do
+
+  user = FactoryGirl.create(:user)
+
+  visit root_path
+  click_on "Sign in"
+save_and_open_page
+
+
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_on "Log in"
+
+
+expect(page).to_not have_content "Sign up"
+
+
+end
+
+
+
+
+
+
+
+
+
+
 
 end
 
