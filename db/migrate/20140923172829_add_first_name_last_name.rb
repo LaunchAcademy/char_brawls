04876a0639802c7c578c620da8ede1c758427e0a
Henry Neels <foglaboratories@gmail.com>
2014-09-23 15:55:22 -0400

    Responded to pull request - deleted migration etc

--- db/migrate/20140923172829_add_first_name_last_name.rb
+++ db/migrate/20140923172829_add_first_name_last_name.rb
@@ -1,7 +1,7 @@
 class AddFirstNameLastName < ActiveRecord::Migration
   def change
-    add_column :users, :first_name, :string
+    add_column :users, :first_name, :string, null: false
 
-    add_column :users, :last_name, :string
+    add_column :users, :last_name, :string,  null: false
   end
 end
