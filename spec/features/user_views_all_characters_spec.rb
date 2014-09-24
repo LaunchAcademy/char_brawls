58a2d7ae6d49d1306467e9a69b94d2219e850813
Ryan <mcmurrayre@icloud.com>
2014-09-24 13:32:22 -0400

    Made Changes

--- spec/features/user_views_all_characters_spec.rb
+++ spec/features/user_views_all_characters_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
-feature "user wants to see character details", %{
+feature 'user wants to see character details', %{
   As an user, I want to see information about my character
 }  do
 
-  scenario "user can view character details" do
+  scenario 'user can view character details' do
     character1 = FactoryGirl.create(:character)
     visit character_path(character1)
     expect(page).to have_content character1.name
   end
 
-  scenario "must link to individual matchups" do
+  scenario 'must link to individual matchups' do
     visit 'characters#show'
     expect(page).to have_content character.matchups
   end
