33103b48a53e4dee16734d4345b0cd118a46c178
Joe Grady <jogrady1@gmail.com>
2014-09-24 16:56:57 -0400

    Resolved merge conflict withremote  master

--- spec/factories.rb
+++ spec/factories.rb
@@ -14,13 +14,10 @@ FactoryGirl.define do
       "#{n}api/v1/pokemon/"
     end
   end
-<<<<<<< HEAD
 
 factory :matchup do
     character_id 1
     opponent_id 2
   end
 
-=======
->>>>>>> 420c6d687c65df4dbe0981836274ebc467b0ee61
 end
