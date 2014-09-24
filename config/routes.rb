deca689fc45a52611a2a7ce6541d22318fcf7049
Joe Grady <jogrady1@gmail.com>
2014-09-24 17:05:35 -0400

    Removed auto-merge detritus

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_out:'sign_out' }
 
-<<<<<<< HEAD
   resources :matchups do
     resources :reviews, only: [:new, :create]
   end
-=======
+
   root to: 'characters#index'
 
   resources :characters, only: [:index, :show]
->>>>>>> 420c6d687c65df4dbe0981836274ebc467b0ee61
 end
