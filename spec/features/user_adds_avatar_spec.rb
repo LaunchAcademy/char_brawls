d1fe3a7553df9a63f71cf9ca4bec227a643cd29a
rachelbookhout <rbookhou@gmail.com>
2014-09-25 11:38:45 -0400

    Started writing Carrier Wave tests

---
+++ spec/features/user_adds_avatar_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+feature 'user add an avatar', %Q(
+  As an user of a site,
+  I want to add an avatar
+  to show people who I am
+) do
+
+  scenario 'user uploads an avatar' do
+
+
+
+
+end
