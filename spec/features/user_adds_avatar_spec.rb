a35621012feb941028fe3f500a8370cc95af6c7f
rachelbookhout <rbookhou@gmail.com>
2014-09-25 13:29:47 -0400

    Added tests

--- spec/features/user_adds_avatar_spec.rb
+++ spec/features/user_adds_avatar_spec.rb
@@ -7,7 +7,18 @@ feature 'user add an avatar', %Q(
 
   scenario 'user uploads an avatar' do
 
-
-
-
+  user = FactoryGirl.build(:user)
+    visit new_user_registration_path
+    fill_in "First name", with: user.first_name
+    fill_in "Last name", with: user.last_name
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password
+    attach_file 'user_avatar','/spec/images/pitbullpuppy.jpg'
+    within '.new_user' do
+      click_on "Sign up"
+    end
+    expect(page).to have_content('Sign Out')
+    expect(page).to have_content(user.profile_photo)
+end
 end
