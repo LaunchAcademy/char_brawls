114cada50fc12863811e3009b56d1cae46e4ed00
Helen Hood <helen.c.hood@gmail.com>
2014-09-25 15:02:37 -0400

    Dry up test

--- spec/features/user_views_all_characters_spec.rb
+++ spec/features/user_views_all_characters_spec.rb
@@ -1,19 +1,14 @@
 require 'rails_helper'
 feature 'user wants to see character details', %{
   As an user, I want to see information about my character
-}  do
-
-  let(:character) { FactoryGirl.create(:character) }
-  let!(:matchups) { FactoryGirl.create_list(:matchup, 3, character: character) }
+  }  do
 
   scenario 'user can view character details' do
+    character = FactoryGirl.create(:character)
+    matchups = FactoryGirl.create_list(:matchup, 3, character: character)
     visit character_path(character)
+
     expect(page).to have_content character.name
-  end
-
-  scenario "must link to individual matchups" do
-    visit character_path(character)
-
     character.matchups.each do |matchup|
       expect(page).to have_content matchup.opponent.name
     end
