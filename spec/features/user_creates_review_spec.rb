2595833c657e247202fd4821e78441ca2793c3e8
Helen Hood <helen.c.hood@gmail.com>
2014-09-25 15:48:15 -0400

    Refactor user creates review spec

--- spec/features/user_creates_review_spec.rb
+++ spec/features/user_creates_review_spec.rb
@@ -1,35 +1,46 @@
 require 'rails_helper'
 feature "Authenticated user creates a review", %{
-As an Authenticated user,
-I want to write a review
-so that I can tell the world about my opinions!
-}  do
+  As an Authenticated user,
+  I want to write a review
+  so that I can tell the world about my opinions!
+  }  do
 
-  scenario "User writes and submits a review" do
-    matchup = FactoryGirl.create(:matchup)
-    visit matchup_path(matchup)
+  context "authenticated user" do
+    before :each do
+      @user = FactoryGirl.create(:user)
+      sign_in_as(@user)
+    end
 
-    fill_in "Battle Notes", with: "I think that link is gonna kick that ass!"
-    click_on "Submit"
+    scenario "User writes and submits a review" do
+      matchup = FactoryGirl.create(:matchup)
+      visit matchup_path(matchup)
+      # Later, test how the user would navigate to this page, something like:
+      # visit root_path
+      # click_on "Browse Matchups"
+      # click_on matchup.description  # description would be an instance method
+      # on matchup, that returned some description like "Pikachu vs. Link"
 
-    expect(page).to have_content "The review has been created successfully."
+      fill_in "Battle Notes", with: "I think that link is gonna kick that ass!"
+      click_on "Submit"
+
+      expect(page).to have_content "The review has been created successfully."
+    end
+
+    scenario "User can't submit an empty review" do
+      matchup = FactoryGirl.create(:matchup)
+      visit matchup_path(matchup)
+      click_on "Submit"
+      expect(page).to have_content "The review can't be empty!"
+    end
+
+    scenario "User can't submit exactly the same review text" do
+      matchup = FactoryGirl.create(:matchup)
+      visit matchup_path(matchup)
+      fill_in "Battle Notes", with: "I think that link is gonna kick that ass!"
+      click_on "Submit"
+      expect(page).to have_content "That review already exists!"
+    end
   end
 
-  scenario "User can't submit an empty review" do
-
-    matchup = FactoryGirl.create(:matchup)
-    visit matchup_path(matchup)
-    click_on "Submit"
-    expect(page).to have_content "The review can't be empty!"
-
-  end
-
-  scenario "User can't submit exactly the same review text" do
-
-    matchup = FactoryGirl.create(:matchup)
-    visit matchup_path(matchup)
-    fill_in "Battle Notes", with: "I think that link is gonna kick that ass!"
-    click_on "Submit"
-    expect(page).to have_content "That review already exists!"
-  end
+  scenario "Unauthenticated user cannot create a review"
 end
