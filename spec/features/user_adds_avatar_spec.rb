6bac5576cd4511a56f080423b4d7d245910ded0d
rachelbookhout <rbookhou@gmail.com>
2014-09-25 16:15:52 -0400

    Happy path test

--- spec/features/user_adds_avatar_spec.rb
+++ spec/features/user_adds_avatar_spec.rb
@@ -7,18 +7,17 @@ feature 'user add an avatar', %Q(
 
   scenario 'user uploads an avatar' do
 
-  user = FactoryGirl.build(:user)
+    user = FactoryGirl.build(:user)
     visit new_user_registration_path
     fill_in "First name", with: user.first_name
     fill_in "Last name", with: user.last_name
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     fill_in "Password confirmation", with: user.password
-    attach_file "Profile Photo","app/assets/images/pitbull_puppy.jpg"
+    attach_file 'Profile photo', 'app/assets/images/pitbull_puppy.jpg'
     within '.new_user' do
       click_on "Sign up"
     end
-    expect(page).to have_content('Sign Out')
     expect(page).to have_content(user.profile_photo)
-end
+    end
 end
