7f96dce085b386b202ab202bf3a54426c4492755
Helen Hood <helen.c.hood@gmail.com>
2014-09-29 11:14:00 -0400

    Add comment for uniqueness validation on opinion

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -6,6 +6,7 @@ class Opinion < ActiveRecord::Base
   validates :body,
     presence: { message: "The opinion can't be empty!" },
     uniqueness: { message: "That opinion already exists!" }
+  # validates :user, uniqueness: { scope: matchup } (also need uniqueness constraint on DB)
   validates :winner, presence: { message: "You must vote!" }
   validate :winner_in_matchup
 
