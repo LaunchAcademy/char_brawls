b09795c78844a4659d51e9c4ec7b61782da9af3c
rachelbookhout <rbookhou@gmail.com>
2014-09-29 11:33:21 -0400

    Comitting Changes before Merge

--- spec/features/user_views_all_characters_spec.rb
+++ spec/features/user_views_all_characters_spec.rb
@@ -4,12 +4,13 @@ feature 'user wants to see character details', %{
 }  do
 
   scenario 'user can view character details' do
-    character1 = FactoryGirl.create(:character)
-    visit character_path(character1)
-    expect(page).to have_content character1.name
+    @character = FactoryGirl.create(:character)
+    visit character_path(@character)
+    expect(page).to have_content @character.name
   end
 
   scenario "must link to individual matchups" do
+    character1 = FactoryGirl.create(:character)
     visit character_path(character1)
     expect(page).to have_content character.matchups
   end
