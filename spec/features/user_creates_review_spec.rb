e5ddd22b7fc03c0d1264bf840e3fad29268b29f2
Joe Grady <jogrady1@gmail.com>
2014-10-01 14:09:35 -0400

    screw this bull, it is back up to date now

--- spec/features/user_creates_review_spec.rb
+++ spec/features/user_creates_review_spec.rb
@@ -14,16 +14,17 @@ feature "Authenticated user creates a opinion", %{
     scenario "User writes and submits a opinion" do
       matchup = FactoryGirl.create(:matchup)
       visit matchup_path(matchup)
+      ActionMailer::Base.deliveries = []
       # Later, test how the user would navigate to this page, something like:
       # visit root_path
       # click_on "Browse Matchups"
       # click_on matchup.description  # description would be an instance method
       # on matchup, that returned some description like "Pikachu vs. Link"
-
       fill_in "Battle Notes", with: "I think that link is gonna kick that ass!"
       choose 'opinion_winner_id_' + matchup.character.id.to_s
       click_on "Create Opinion"
-
+      expect(ActionMailer::Base.deliveries.size).to eql(1)
+      ActionMailer::Base.deliveries.last
       expect(page).to have_content "The opinion has been created successfully."
     end
 
@@ -53,9 +54,9 @@ feature "Authenticated user creates a opinion", %{
     end
   end
 
-  scenario "Unauthenticated user cannot create a opinion" do
-    matchup = FactoryGirl.create(:matchup)
-    visit matchup_path(matchup)
+    scenario "Unauthenticated user cannot create a opinion" do
+      matchup = FactoryGirl.create(:matchup)
+      visit matchup_path(matchup)
 
     expect(page).not_to have_content "Battle Notes"
   end
